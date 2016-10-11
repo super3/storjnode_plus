6ac249deb04aec8aee4ce6c4152153faae143548
Christopher Evans <aaxiomfinity@gmail.com>
2016-10-10 21:41:58 -0700

    reboot mode

--- chkshards.sh
+++ chkshards.sh
@@ -39,6 +39,6 @@ if pgrep "storjshare" > /dev/null
             echo "Storjshare is not running." >> /var/www/html/status.txt
             echo "Storjshare is not running." >> /home/chris/Desktop/offline.txt
             cat /home/chris/Desktop/offline.txt | mailx -s "Node 1 statistics analysis"  axiomfinity@netzero.com
-            # shutdown -r now
+            shutdown -r now
 fi 
 
